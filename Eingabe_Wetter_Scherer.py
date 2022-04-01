fa5eee03e5694c2d17b8f27c89d68c630c769d18
FlorentinaScherer <102793424+FlorentinaScherer@users.noreply.github.com>
2022-04-01 11:20:30 +0200

    Add files via upload

---
+++ Eingabe_Wetter_Scherer.py
@@ -0,0 +1,15 @@
+
+
+wetter = input("Eingabe Wettermöglichkeit: 'sonnig' oder 'regnerisch':")
+
+if wetter.upper() == "REGNERISCH":
+    print("Kellerparty")
+    print("Mist")
+elif wetter.lower() == "sonnig":
+    print("Gartenparty")
+    print("Jippiieeeh")
+else:
+    print("bitte 'sonne' oder 'regen' eingeben")
+    
+    
+
